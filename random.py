08bb6dd908255901cf6d984bd62f8b8cf15805e4
Prasun Singh <prasun.singh@verts.co.in>
2017-09-22 11:09:03 +0530

    Initial commit

---
+++ random.py
@@ -0,0 +1,22 @@
+def randint(a, b):
+    "Return random integer in range [a, b], including both end points."
+    return a + randbelow(b - a + 1)
+
+def randbelow(n):
+    "Return a random int in the range [0,n).  Raises ValueError if n<=0."
+    if n <= 0:
+       raise ValueError
+    k = n.bit_length()
+    numbytes = (k + 7) // 8
+    while True:
+        r = int.from_bytes(random_bytes(numbytes), 'big')
+        r >>= numbytes * 8 - k
+        if r < n:
+            return r
+
+def random_bytes(n):
+    "Return n random bytes"
+    with open('/dev/urandom', 'rb') as file:
+        return file.read(n)
+if __name__=="__main__":
+	print(randint(1, 1000))
